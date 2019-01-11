7380f029775ffaacd986574f703bb1dd66fc2d63
Jrew <jrewmohamed@gmail.com>
2019-01-10 19:54:24 -0500

    Updates

---
+++ precache-manifest.39f0cd2f23be5e14d51d5b00fbd99c4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "80f0f94e83622e00e717",
+    "url": "/dragon-dropper/static/js/runtime~main.80f0f94e.js"
+  },
+  {
+    "revision": "a2a767ac9d0b72f56825",
+    "url": "/dragon-dropper/static/js/main.a2a767ac.chunk.js"
+  },
+  {
+    "revision": "0847d3688c72634432d2",
+    "url": "/dragon-dropper/static/js/1.0847d368.chunk.js"
+  },
+  {
+    "revision": "a2a767ac9d0b72f56825",
+    "url": "/dragon-dropper/static/css/main.0246dc6e.chunk.css"
+  },
+  {
+    "revision": "92a734adc000bfc8a3cbecdf6fba80ca",
+    "url": "/dragon-dropper/index.html"
+  }
+];
